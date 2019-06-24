277c00d8e94b4fddbc75521e3033cfc82bc90a7b
QingShanLuoYue <980575067@qq.com>
2019-06-24 21:53:28 +0800

    fix

--- example/render.js
+++ example/render.js
@@ -1,14 +1,40 @@
 with(this) {
-	return _c('div', {
-		staticClass: 'login'
-	}, [_c('LoginTab', {
-		ref: 'loginTab'
-	}), _v(' '), _c('LoginByAcount', {
-		ref: 'loginByAcount',
-		attrs: {
-			allDisabled: allDisabled
-		}
-	}), _v(' '), _c('div', {
-		staticClass: 'other-opr'
-	}, [_v(_s(_f('filter2')(_f('filter1')(otherVar))))])], 1)
+    return _c(
+            'div', 
+            {
+                staticClass: 'login'
+            }, 
+            [
+                _c('LoginTab', 
+                    {
+                        ref: 'loginTab'
+                    }
+                ), 
+                _v(' '), 
+                _c(
+                    'LoginByAcount', 
+                    {
+                        ref: 'loginByAcount',
+                        attrs: {
+                            allDisabled: allDisabled
+                        }
+                    }
+                ), 
+                _v(' '), 
+                _c(
+                    'div',
+                    {
+                        staticClass: 'other-opr'
+                    }, 
+                    [
+                        _v(
+                            _s(
+                                _f('filter2')(_f('filter1')(otherVar))
+                            )
+                        )
+                    ]
+                )
+            ], 
+            1
+        )
 }
